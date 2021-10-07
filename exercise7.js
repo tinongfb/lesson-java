cf288793edf00f58be7ef0d6421f01e28d7b9096
tinongfb <tinongfb@gmail.com>
2021-10-07 15:47:40 +0800

    ztm javascript exercise 7

---
+++ exercise7.js
@@ -0,0 +1,29 @@
+var database = [
+    {
+        username: "tin1",
+        password: "123",
+    },
+    {
+        username: "tin2",
+        password: "234",
+    },
+    {
+        username: "tin3",
+        password: "345",
+    }
+];
+
+var newsfeed = [
+    {
+        username: "alphen",
+        timeline: "I can't beat the level 70 dragon ^^;",
+    },
+    {
+        username: "shionne",
+        timeline: "Time to practice for the perfect pancake.",
+    },
+    {
+        username: "kisara",
+        timeline: "dropping by the grocery, does anybody have request for supper?",
+    }
+]
