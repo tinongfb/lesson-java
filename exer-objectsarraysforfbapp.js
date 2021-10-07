b724759775b8baa484c037d20e424251d716fd59
tinongfb <tinongfb@gmail.com>
2021-10-07 16:00:00 +0800

    renamed files exercise7

--- exer-objectsarraysforfbapp.js
+++ exer-objectsarraysforfbapp.js
@@ -1,3 +1,14 @@
+// Exercise: OBJECTS AND ARRAYS FOR FACEBOOK APP
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+var sample = {
+    username: "tin1",
+    password: "123",
+}
+
+// 2. Create an array which contains the object you have made above and name the arry "database"
+
 var database = [
     {
         username: "tin1",
@@ -13,6 +24,7 @@ var database = [
     }
 ];
 
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
 var newsfeed = [
     {
         username: "alphen",
