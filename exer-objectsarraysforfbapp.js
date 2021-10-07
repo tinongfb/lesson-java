5e74d9c45c9bbd199377f4c70955eee8614cf53f
tinongfb <tinongfb@gmail.com>
2021-10-07 18:44:51 +0800

    finished lecture 11-17

--- exer-objectsarraysforfbapp.js
+++ exer-objectsarraysforfbapp.js
@@ -3,39 +3,51 @@
 
 // 1. Create an object that has properties "username" and "password". Fill those values in with strings.
 var sample = {
-    username: "tin1",
+    username: "tin",
     password: "123",
-}
+};
 
 // 2. Create an array which contains the object you have made above and name the arry "database"
 
 var database = [
     {
         username: "tin1",
-        password: "123",
+        password: "abc",
     },
     {
         username: "tin2",
-        password: "234",
-    },
-    {
-        username: "tin3",
-        password: "345",
+        password: "xyz",
     }
 ];
 
 // 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
-var newsfeed = [
+var newsFeed = [
     {
-        username: "alphen",
-        timeline: "I can't beat the level 70 dragon ^^;",
+        username: "Alphen",
+        timeline: "I can't beat the level 70 dragon ^^;"
     },
     {
-        username: "shionne",
-        timeline: "Time to practice for the perfect pancake.",
+        username: "Shionne",
+        timeline: "Time to practice for the perfect pancake."
     },
     {
         username: "kisara",
-        timeline: "dropping by the grocery, does anybody have request for supper?",
+        timeline: "dropping by the grocery, does anybody have request for supper?"
     }
-]
+];
+
+// exercise: making a facebook app
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+function signIn(user, pass) {
+    if (user === database[0].username &&
+        pass === database[0].password) {
+        console.log(newsFeed);
+    } else {
+        alert("Sorry, wrong username and password!");
+    }
+}
+
+signIn(userNamePrompt, passwordPrompt);
